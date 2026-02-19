fef1e838000eb095628f24085446b691bb64107c
SohailaN12 <sohaila.noori2027@gbwl.org>
2026-02-19 19:25:14 +0000

    4.7 notes

--- Main.java
+++ Main.java
@@ -1,6 +1,31 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+
+      // 4.7 WRAPPER CLASSES wrap primitives into objects 
+      // why? some methods NEED objects!
+      Integer fancyInt = new Integer(2);
+      Double fancyDoub = new Double(6.00);
+      // calling the constructor is no longer necessary!
+      // can just declare them like you would primitives:
+      Integer fancierInt = 13; 
+      System.out.println(fancierInt);
+      // AUTOBOXING:  Java converts int to Integer 
+      int regularInt = fancierInt; // UNBOXING
+
+      // Another benefit of Wrappers: you can use some methods
+      System.out.println(Integer.MIN_VALUE);
+      System.out.println(Integer.MIN_VALUE);
+      String stringgyNums = "2.0";
+      double parsedD = Double.parsedDouble(stringgyNums);
+      System.out.println(parsedD);
+      String stringgyNums2 = "2";
+      int parsedI = Integer.parsedInt(stringgyNums2);
+      System.out.println(parsedI);
+
+
+
+
+
    }
 }
